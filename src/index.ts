decb28b27ba3a68ab51079aa29b959cf1016b4f9
Jonas Wijne <jonas.wijne@jimber.org>
2020-02-03 15:43:03 +0100

    add working keypair generation

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,44 @@
 import {SiaBinaryEncoder} from "./tfchain/tfchain.encoding.siabin";
-import {base64Decode, base64Encode, blake2b} from "@waves/ts-lib-crypto";
+import {base64Encode, blake2b} from "@waves/ts-lib-crypto";
 import {mnemonicToEntropy} from "bip39";
 import {sign_keyPair_fromSeed} from "tweetnacl-ts";
+import {ByteArray} from "tweetnacl-ts/src/array";
 
-const seedPhrase: string = "weird derive razor only rally vault pulse live ignore wet kick tonight quiz host always vintage inmate garbage cherry blue target check february slide";
+// const seedPhrase: string = "weird derive razor only rally vault pulse live ignore wet kick tonight quiz host always vintage inmate garbage cherry blue target check february slide";
+const seedPhrase: string = "intact planet sunset mad possible peanut enrich hollow reopen cannon silk pledge double spot stick lyrics clean wagon upgrade cluster van picnic steel master";
 
-const entropy = mnemonicToEntropy(seedPhrase);
+const entropy: string = mnemonicToEntropy(seedPhrase);
 
-const seed: Uint8Array = base64Decode(entropy);
+const convertHexstringToEntropy: (hexString: string) => Uint8Array = (hexString: string) => {
+    return new Uint8Array(
+        hexString
+            .match(/.{1,2}/g)
+            .map(
+                byte => parseInt(byte, 16)
+            )
+    );
+};
+
+const seed: Uint8Array = convertHexstringToEntropy(entropy);
 
 
-for (let i = 0; i < 3; i++) {
+type KeyPair = {
+    publicKey: ByteArray;
+    secretKey: ByteArray;
+};
+
+for (let i = 0; i < 4; i++) {
     const encoder = SiaBinaryEncoder();
 
     encoder.add_array(seed);
     encoder.add_int(i);
 
-    // console.log(encoder.data);
     // h in go file
-    const blake2b1Hash = blake2b(encoder.data);
+    const blake2b1Hash: Uint8Array = blake2b(encoder.data);
 
-    console.log(`hash ${i}:"${base64Encode(blake2b1Hash)}"`);
-    const keyPair = sign_keyPair_fromSeed(blake2b1Hash);
+    // console.log(`hash ${i}:"${base64Encode(blake2b1Hash)}"`);
+    const keyPair: KeyPair = sign_keyPair_fromSeed(blake2b1Hash);
     console.log(`pubkey:"${base64Encode(keyPair.publicKey)}"`)
-
+    // console.log(`secretKey:"${base64Encode(keyPair.secretKey)}"`)
 }
 
